8c52b878697df7c719a2b6f96708f9b917691755
Elijah Dodge <ej_6612@outlook.com>
2023-06-20 18:35:15 -0600

    Home and Project Cards -Home page structure (more of) -Two project cards created -Project pages are loaded into an iframe via html files

---
+++ scripts/project_frame.js
@@ -0,0 +1,40 @@
+// Get a reference to the iframe element
+const iframe = document.getElementById('projectsFrame');
+
+// Get references to the arrow buttons
+const prevButton = document.getElementById('prevButton');
+const nextButton = document.getElementById('nextButton');
+
+// Define an array of source URLs for the iframe
+const sources = [
+  'projects/project-cards/easequations.html',
+  'projects/project-cards/silentInq.html',
+  // Add more URLs as needed
+];
+
+let currentIndex = 0;
+
+// Function to update the iframe src
+function updateIframeSrc() {
+  iframe.src = sources[currentIndex];
+}
+
+// Event listener for the previous button
+prevButton.addEventListener('click', () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+  } else {
+    currentIndex = sources.length - 1;
+  }
+  updateIframeSrc();
+});
+
+// Event listener for the next button
+nextButton.addEventListener('click', () => {
+  if (currentIndex < sources.length - 1) {
+    currentIndex++;
+  } else {
+    currentIndex = 0;
+  }
+  updateIframeSrc();
+});
