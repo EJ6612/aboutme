dc465cc1971a19f9203bed401a9e1488a3ccec65
Elijah Dodge <ej_6612@outlook.com>
2023-07-11 18:47:01 -0600

    Random Elijah code

---
+++ scripts/random_elijah.js
@@ -0,0 +1,22 @@
+// Array of image URLs
+var imageUrls = [
+    "../contact/images/elijah/2023-easter.jpg",
+    "../contact/images/elijah/2021-temple.jpg",
+    "../contact/images/elijah/2023-easter.jpg",
+    "../contact/images/elijah/2022-trash.jpg",
+    "../contact/images/elijah/2023-easter.jpg",
+    "../contact/images/elijah/2023-style.jpeg",
+    "../contact/images/elijah/2023-easter.jpg",
+    "../contact/images/elijah/2023-yellowstone.jpg",
+    "../contact/images/elijah/2023-easter.jpg"
+    // Add more image URLs here
+];
+
+// Generate a random index
+var randomIndex = Math.floor(Math.random() * imageUrls.length);
+
+// Get the img element by its id
+var imgElement = document.getElementById("elijah-random");
+
+// Set the src attribute with the randomly selected image URL
+imgElement.src = imageUrls[randomIndex];
