2fa985fb7a6280319e076a1bbffce6b67a6e522c
ej6612 <ej_6612@outlook.com>
2024-12-07 19:10:50 -0700

    footer fix

--- scripts/loadFooter.js
+++ scripts/loadFooter.js
@@ -2,7 +2,7 @@
 const footerPlaceholder = document.getElementById('footer');
 
 // Fetch and insert the footer.html content
-fetch('/footer.html')
+fetch('../footer.html')
     .then(response => {
         if (!response.ok) {
             throw new Error('Failed to load footer');
